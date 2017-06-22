4ff81b5d63c2c4f0a17669209570f35df05c173d
lee brian <leesinshingbrian@gmail.com>
2017-06-22 20:27:39 +0800

    TRIED MY BEST

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+document.getElementById('startButton').addEventListener('click', guessedNum)
+
 // update the h1 to say higher or lower
 // or if the number guess is correct, we change the body color
 
 var randomNumber = randomFn(0, 10)
-var guessedNum = prompt('The first guess: type your number!')
 var isCorrect = false
-var gameOver = false
+var gameOver = true
+var h1 = document.querySelector('h1')
+var number;
 
+function guessedNum (input) {
+  input = prompt('Please key in a number')
+  guessedNum = parseInt(input)
+  askForANumber()
+}
 // fn that generates randomNum
 // min and max always whole number
 // random number between min and max (exclusive)
@@ -13,44 +21,36 @@ function randomFn (min, max) {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
-// this fn. updates h1 text content
-function updateH1(newH1) {
-  var h1 = document.querySelector('h1')
-  // var h1 = document.getElementsByTagName('h1')
-
-  // updated the property of h1 dom node
-  h1.textContent = newH1
-}
-
 // fn that checks numbers with the random number
 function askForANumber (guessedNum, randomNumber) {
   if (guessedNum > randomNumber) {
-    updateH1('lower, randomNumber is ' + randomNumber)
+    h1.style.color = 'red'
   } else if (guessedNum < randomNumber) {
-    updateH1('higher, randomNumber is ' + randomNumber)
-  } else if(guessedNum === randomNumber) {
-    updateH1('correct')
-    gameOver = true
+    h1.style.color = 'white'
+  } else if (guessedNum === randomNumber) {
+    h1.style.color = 'green'
+    // gameOver = true
   }
-  checkForGameover()
+  // checkForGameover()
 }
 
-function newGame() {
+function newGame () {
   // setting randomNumber to 5 for easy checking
-  askForANumber( guessedNum, 5 )
+  askForANumber(guessedNum, 5)
 }
 
-function checkForGameover() {
+function checkForGameover () {
   if(gameOver) {
-    alert('reveal the restart button now, stop the game')
+    alert("That's right!")
   } else {
-    guessedNum = prompt('Type your number again!')
-    // bugs?!
+    gameOver = false
+    var guessNum = prompt('Please key in a number')
+    var guessedNum = parseInt(guessNum)
+        // bugs?!
     askForANumber(guessedNum, 5)
   }
 }
 
-// bugs?!
 if(! gameOver) {
   newGame()
 }
